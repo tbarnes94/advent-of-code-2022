6c9a1173a8cd4a95643e445704e3d471c37f476b
Tommy Barnes <thbarnes@microsoft.com>
2022-12-20 23:04:24 -0600

    day 1 complete

--- 1/1.go
+++ 1/1.go
@@ -3,13 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
+
+func tallyElfCalories(stack string) int {
+	arr := strings.Split(stack, "\n")
+	sum := 0
+	for _, value := range arr {
+		intVal, _ := strconv.Atoi(value)
+		sum = sum + intVal
+	}
+	return sum
+}
 
 func main() {
 	content, _ := ioutil.ReadFile("input.txt")
 	sc := string(content)
-	arr := strings.Split(sc, "\n")
+	arr := strings.Split(sc, "\n\n")
 
-	fmt.Println("Something to print:", arr)
+	maxCalories := 0
+	for _, stack := range arr {
+		calories := tallyElfCalories(stack)
+		if maxCalories < calories {
+			maxCalories = calories
+		}
+	}
+
+	fmt.Println("Something to print:", maxCalories)
 }
